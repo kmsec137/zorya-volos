29f66a3ed8efe561fb7c56a0cbef445504f699b3
kminfosec <kminfosec@gmail.com>
2026-01-28 21:08:43 +0200

    [working commit] debugging the volos log prompt issues

--- src/state/memory_x86_64.rs
+++ src/state/memory_x86_64.rs
@@ -767,7 +767,7 @@ impl<'ctx> MemoryX86_64<'ctx> {
 												AccessType::Write,
 												volos.locks_held);
 
-			println!("[VOLOS] writing memory with volos --> @[0x{:X}] {:?}", address, new_volos);
+			println!("[VOLOS] writing memory --> @[0x{:X}] <Volos( thread_id:{:?} access_type:{:?} locks_held:{:#?} )>", address, new_volos.thread_id, new_volos.access_type, new_volos.locks_held);
 		  
         let mut regions = self.regions.write().unwrap();
         // Check if the address falls within an existing memory region
