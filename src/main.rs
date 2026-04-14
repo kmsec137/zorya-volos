4f58979c66b0616c876aedc66f4199aa215fc342
kminfosec <kminfosec@gmail.com>
2026-04-14 11:33:34 +0200

    [working commit] trying some patterns to fix the lock and other symbol arg treatment

--- src/main.rs
+++ src/main.rs
@@ -1245,8 +1245,7 @@ fn execute_instructions_from(
 		  let mut is_concurrent: bool = false;
 
 
-			let symbol_name_owned: Option<String> = executor.symbol_table.get(&current_rip_hex).cloned();
-			
+			/*	
 			let is_concurrent = if let Some(ref name) = symbol_name_owned {
 			    name == "sym.runtime.lock" || name == "runtime.lock2"
 			        || name == "runtime.unlock" || name == "runtime.unlock2"
@@ -1269,7 +1268,7 @@ fn execute_instructions_from(
 			        current_tid, 
 			        executor.main_vecclock
 			    );
-			}
+			}*/
 
 
 /*
@@ -1301,40 +1300,167 @@ fn execute_instructions_from(
 
 			
 */
-        if let Some(symbol_name) = symbol_name_owned {
-        		
-            if symbol_name == "runtime.unlock2" {
-              if let Some((_, args)) = function_args_map.get(&current_rip) {
-						let cpu = executor.state.cpu_state.lock().unwrap();
-						for (_arg_name, reg_names, _arg_type) in args {
-						    for reg_name in reg_names {
-						        if let Some(offset) = cpu.resolve_offset_from_register_name(reg_name) {
-						            if let Some(value) = cpu.get_register_by_offset(offset, 64) {
-											//println!("[VOLOS::main.rs] got lock function call {:?} mutex={:?}",symbol_name,args,arg_name, value.concrete)
-											//println!("[VOLOS::main.rs] got unlock function call {:?} mutex=0x{:x}",symbol_name, value.concrete);
-            						    println!("[ZORYA @<0x{:x}>] encountered {} operation args -> {:?}",current_rip, symbol_name, value.concrete); 
+		 let symbol_name_extract = executor.symbol_table.get(&current_rip_hex).cloned();
+		 let current_tid: Option<u64>= {
+		 		let thread_manager = executor.state.thread_manager.lock().unwrap();
+		 		Some(thread_manager.current_tid)
+		 };
 
-											let mut thread_manager = executor.state.thread_manager.lock().unwrap();
-										   let current_tid = thread_manager.current_tid;
-										   let mut current_thread: &mut OSThread<'_> = thread_manager.current_thread_mut().unwrap();
-											let mut locks: &mut Vec<u64> = current_thread.locks_held.borrow_mut();
+	//tick the clock
 
-											locks.retain(|&x| x != value.concrete.to_u64());
-											let len = locks.len();
+		if let (Some(name), Some(tid)) = (symbol_name_extract, current_tid) {
+		    let is_concurrent = name == "sym.runtime.lock" || name == "runtime.lock2" 
+		        || name == "runtime.unlock" || name == "runtime.unlock2"
+		        || name == "runtime.chansend" || name == "runtime.newproc1";
+		
+		    if is_concurrent {
+		        println!("[VOLOS] Tick for {} (TID: {:?})", name, tid);
+		        
+		        // 3. MUTABLE CALL
+		        // This now works because 'symbol_table' is no longer borrowed 
+		        // and the 'thread_manager' lock was dropped above.
+		        //executor.tick_vc(&tid.expect("current tid unable to extract :(").to_string());
+		        executor.tick_vc(&tid.to_string());
 
-											//println!("[VOLOS::main.rs] thread[{}].locks_held -> {:#?}",current_tid, locks);
-						            }
-						        }
-						    }
-						}
+				 // TEST PASTE BEGIN
+
+								if name == "runtime.unlock2" {
+			   		 		println!(
+			   		 	    "[VOLOS] ticked the vc clock for thread:{:?} --> vc:{}",
+			   		 	    current_tid, 
+			   		 	    executor.main_vecclock
+			   		 		);
+
+            		 	 if let Some((_, args)) = function_args_map.get(&current_rip) {
+        				 			println!("[ZORYA @<0x{:x}>] runtime.lock2 check {:?}",current_rip, args); 
+						 	  	let cpu = executor.state.cpu_state.lock().unwrap();
+						 	  	for (_arg_name, reg_names, _arg_type) in args {
+						 	  	    for reg_name in reg_names {
+						 	  	        if let Some(offset) = cpu.resolve_offset_from_register_name(reg_name) {
+						 	  	            if let Some(value) = cpu.get_register_by_offset(offset, 64) {
+						 	  						//println!("[VOLOS::main.rs] got lock function call {:?} mutex={:?}",symbol_name,args,arg_name, value.concrete)
+						 	  						//println!("[VOLOS::main.rs] got unlock function call {:?} mutex=0x{:x}",symbol_name, value.concrete);
+            		 	  					    println!("[ZORYA @<0x{:x}>] encountered {} operation args -> {:?}",current_rip, name, value.concrete); 
+
+				 	  							   let mut _thread_manager = executor.state.thread_manager.lock().unwrap();
+						 	  					   let mut current_thread: &mut OSThread<'_> = _thread_manager.current_thread_mut().unwrap();
+						 	  						let mut locks: &mut Vec<u64> = current_thread.locks_held.borrow_mut();
+
+						 	  						locks.retain(|&x| x != value.concrete.to_u64());
+						 	  						let len = locks.len();
+
+						 	  						//println!("[VOLOS::main.rs] thread[{}].locks_held -> {:#?}",current_tid, locks);
+						 	  	            }
+						 	  	        }
+						 	  	    }
+						 	  	}
+						
+
+						 	  }
+								
+		    				}
+      
+
+
+
+
+							if name == "sym.runtime.lock" || name == "runtime.lock2" {
+					       if let Some((_, args)) = function_args_map.get(&current_rip) {
+			        		 		println!("[ZORYA @<0x{:x}>] runtime.lock2 check {:?}",current_rip, args); 
+					            let cpu = executor.state.cpu_state.lock().unwrap();
+					            for (arg_name, reg_names, _arg_type) in args {
+					         	    for reg_name in reg_names {
+					         	        if let Some(offset) = cpu.resolve_offset_from_register_name(reg_name) {
+					         	            if let Some(value) = cpu.get_register_by_offset(offset, 64) {
+					         	            //println!("[VOLOS::main.rs] got lock function call {:?} mutex={:?}",symbol_name,args,arg_name, value.concrete)
+					         	            //println!("[VOLOS::main.rs] got lock function call {:?} mutex=0x{:x}",symbol_name, value.concrete);
+			            					    println!("[ZORYA @<0x{:x}>] encountered {} operation args -> {:?}",current_rip, name, value.concrete); 
+
+				 	  									 let mut _thread_manager = executor.state.thread_manager.lock().unwrap();
+					         	                let mut current_thread: &mut OSThread<'_> = _thread_manager.current_thread_mut().unwrap();
+					         	                let mut locks: &mut Vec<u64> = current_thread.locks_held.borrow_mut();
+					         	                locks.push(value.concrete.to_u64());
+			                                }
+					         	            //let len = locks.len();
+					         	            //println!("[VOLOS::main.rs] thread[{}].locks_held -> {:#?}",current_tid, locks.get(len - 1));
+														 	
+					         	        }
+					         	    }
+					         	}
+					        }
+            }
+
+
+				// TEST PASTE END
+
+
+
+
+
+
+
+
+
+
+
+
+		    }
+		}
+
+		let symbol_name_extract = executor.symbol_table.get(&current_rip_hex).cloned();
+		if let Some(symbol_name) = symbol_name_extract {
+          	  if symbol_name == "runtime.unlock2" {
+        		 		//println!("[ZORYA @<0x{:x}>] runtime.unlock2 check ",current_rip); 
+				 	  					//drop(thread_manager);
+				 	  
+				 	 //executor.tick_vc(&current_tid.to_string());	
+		       	 //executor.tick_vc(&tid.expect("current tid unable to extract :(").to_string());
+			    		println!(
+			    	    "[VOLOS] ticked the vc clock for thread:{:?} --> vc:{}",
+			    	    current_tid, 
+			    	    executor.main_vecclock
+			    		);
+
+             	 if let Some((_, args)) = function_args_map.get(&current_rip) {
+        		 			println!("[ZORYA @<0x{:x}>] runtime.lock2 check {:?}",current_rip, args); 
+				 	  	let cpu = executor.state.cpu_state.lock().unwrap();
+				 	  	for (_arg_name, reg_names, _arg_type) in args {
+				 	  	    for reg_name in reg_names {
+				 	  	        if let Some(offset) = cpu.resolve_offset_from_register_name(reg_name) {
+				 	  	            if let Some(value) = cpu.get_register_by_offset(offset, 64) {
+				 	  						//println!("[VOLOS::main.rs] got lock function call {:?} mutex={:?}",symbol_name,args,arg_name, value.concrete)
+				 	  						//println!("[VOLOS::main.rs] got unlock function call {:?} mutex=0x{:x}",symbol_name, value.concrete);
+             	  					    println!("[ZORYA @<0x{:x}>] encountered {} operation args -> {:?}",current_rip, symbol_name, value.concrete); 
+
+				 	  						let mut thread_manager = executor.state.thread_manager.lock().unwrap();
+				 	  					   let current_tid = thread_manager.current_tid;
+				 	  					   let mut current_thread: &mut OSThread<'_> = thread_manager.current_thread_mut().unwrap();
+				 	  						let mut locks: &mut Vec<u64> = current_thread.locks_held.borrow_mut();
+
+				 	  						locks.retain(|&x| x != value.concrete.to_u64());
+				 	  						let len = locks.len();
+
+				 	  						//println!("[VOLOS::main.rs] thread[{}].locks_held -> {:#?}",current_tid, locks);
+				 	  	            }
+				 	  	        }
+				 	  	    }
+				 	  	}
 				
 
-				}
+				 	  }
 						
-		    }
+		    	}
       
             if symbol_name == "sym.runtime.lock" || symbol_name == "runtime.lock2" {
+						//let thread_manager = executor.state.thread_manager.lock().unwrap();
+		   		   //let current_tid = thread_manager.current_tid;
+						//drop(thread_manager);
+
+						//executor.tick_vc(&current_tid.to_string());	
+
+        		  //println!("[ZORYA @<0x{:x}>] runtime.lock2 check ",current_rip); 
 		        if let Some((_, args)) = function_args_map.get(&current_rip) {
+        		 		println!("[ZORYA @<0x{:x}>] runtime.lock2 check {:?}",current_rip, args); 
 		            let cpu = executor.state.cpu_state.lock().unwrap();
 		            for (arg_name, reg_names, _arg_type) in args {
 		         	    for reg_name in reg_names {
@@ -1361,6 +1487,9 @@ fn execute_instructions_from(
 
 
 		    if symbol_name == "runtime.makechan" || symbol_name == "runtime.makechan2" {
+				//executor.tick_vc(&current_tid.to_string());
+			   //println!(   "[VOLOS] ticked the vc clock for thread:{} --> vc:{}",current_tid, executor.main_vecclock);
+
 		      if let Some((_, args)) = function_args_map.get(&current_rip) {
 		          let cpu = executor.state.cpu_state.lock().unwrap();
 		          for (arg_name, reg_names, _arg_type) in args {
@@ -1381,6 +1510,7 @@ fn execute_instructions_from(
 
                     
 		    if symbol_name == "runtime.chanrecv" || symbol_name == "runtime.chanrecv2" {
+
 		        if let Some((_, args)) = function_args_map.get(&current_rip) {
 		            let cpu = executor.state.cpu_state.lock().unwrap();
 		            for (arg_name, reg_names, _arg_type) in args {
